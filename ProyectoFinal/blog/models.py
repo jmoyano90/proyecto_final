c0755c0d8022aac417439027d1482d8f6b97f67e
Javier Moyano <javier.moyano1404@gmail.com>
2022-10-07 12:58:58 -0300

    agrego modelos

---
+++ ProyectoFinal/blog/models.py
@@ -0,0 +1,19 @@
+import re
+from django.db import models
+
+# Create your models here.
+class Autor (models.Model):
+    
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    profesion = models.CharField(max_length=30)
+
+class Articulo (models.Model):
+    
+    titulo = models.CharField(max_length=30)
+    texto = models.CharField(max_length=1000)
+    fecha = models.DateTimeField(null=True)
+
+class Autor (models.Model):
+    
+    nombre = models.CharField(max_length=30)
