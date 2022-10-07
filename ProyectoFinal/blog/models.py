47b749f3972e4992c49a522249d7e56ba49eb7fb
Javier Moyano <javier.moyano1404@gmail.com>
2022-10-07 18:56:31 -0300

    agrego formularios y templates

--- ProyectoFinal/blog/models.py
+++ ProyectoFinal/blog/models.py
@@ -1,4 +1,4 @@
-import re
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
@@ -11,7 +11,7 @@ class Autor(models.Model):
     profesion = models.CharField(max_length=30)
 
     def __str__(self):
-        return self.nombre + self.apellido
+        return f"({self.nombre}) {self.apellido}"
 
 
 class Articulo(models.Model):
