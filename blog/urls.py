f21e0c00d730405cf924c1bef43db6934e3a6a49
Iñaki Tuda <inakituda@gmail.com>
2022-10-17 19:36:16 -0300

    agregue urls

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 from blog.views import (
+    buscar_autor,
+    buscar_seccion,
     busqueda_articulo,
     busqueda_seccion,
     mostrar_inicio,
@@ -25,4 +27,6 @@ urlpatterns = [
     path('busqueda_articulo/', busqueda_articulo),
     path('busqueda_seccion/', busqueda_seccion),
     path('buscar_articulo/', buscar_articulo),
+    path('buscar_autor/', buscar_autor),
+    path('buscar_seccion/', buscar_seccion),
 ]
