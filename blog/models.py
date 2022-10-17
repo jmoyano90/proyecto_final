31f599319b85ebf5677faadb4e0f30792081a266
Iñaki Tuda <inakituda@gmail.com>
2022-10-17 19:12:09 -0300

    hice migraciones

--- blog/models.py
+++ blog/models.py
@@ -7,8 +7,8 @@ class Autor(models.Model):
         verbose_name_plural = "Autores"
 
     nombre = models.CharField(max_length=30)
-    apellido = models.CharField(max_length=30)
-    profesion = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30, null=True)
+    profesion = models.CharField(max_length=30, null=True)
 
     def __str__(self):
         return f"({self.nombre}) {self.apellido}"
