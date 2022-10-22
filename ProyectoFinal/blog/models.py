910ffe5b9cd43ed36dfeb89491b5923b13326eed
Javier Moyano <javier.moyano1404@gmail.com>
2022-10-22 18:18:46 -0300

    modificaicon modelos

--- ProyectoFinal/blog/models.py
+++ ProyectoFinal/blog/models.py
@@ -11,7 +11,7 @@ class Autor(models.Model):
     profesion = models.CharField(max_length=30)
 
     def __str__(self):
-        return f"({self.nombre}) {self.apellido}"
+        return f"({self.nombre}) {self.apellido})"
 
 
 class Articulo(models.Model):
